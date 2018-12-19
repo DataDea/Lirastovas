4b1d492ddd7c681203af915370a97946326af541
yanghai <yanghai@ablecloud.cn>
2018-12-20 01:37:04 +0800

    添加IO多路复用模型的C代码实现

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+//epoll头文件
+//#include "epoll/epoll-server.h"
+//#include "epoll/epoll-client.h"
+
+//poll头文件
+#include "poll/poll-server.h"
+#include "poll/poll-client.h"
+
+//select头文件
+#include "select/select-server.h"
+#include "select/select-client.h"
+
+using namespace std;
+
+int main() {
+//    pollserverstart();
+//    pollclientstart();
+    return 0;
+}
